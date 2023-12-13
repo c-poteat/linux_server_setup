d0b3dfe4b9739e3d7de1941fee5ed7a9288453e8
c-poteat <nellp7@yahoo.com>
2023-12-12 20:55:05 -0500

    updated

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# linux_server_setup
+# Linux * Windows Server * Setup
+
+* Environment setup for linux and windows servers as related to winrm connection.
+
+## Description
+
+* The purpose of this project is to automate test environment for linux and windows systems for coding.
+
+## Getting Started
+
+### Dependencies
+
+* Utilizing cloud AWS servers for deployment.
+
+### Executing program
+
+* The initial execution is the result of launching env_start.sh.
+
+## Help
+
+
+
+## Authors
+
+Contributors names and contact info
+
+https://github.com/c-poteat
+
+## Version History
+
+* 0.1 - Initial release
---
+++ env_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Specific repo to enable through RHEL system
+repo_name="ansible-2.9-for-rhel-8-x86_64-rpms"
+
+# Check if repo is already enabled
+if ! dnf repolist | grep -q "$repo_name"; then
+  sudo dnf config-manager -- set-enabled "$repo_name"
+  echo "Repository '$repo_name' enabled."
+else
+  echo "Repository '$repo_name' is already enabled."
+fi
+
+# Install Ansible
+if ! command -v ansible &>/dev/null; then
+  sudo dnf install ansible -y 
+  echo "Ansible was installed successfully."
+  sudo dnf install python3 python3-pip -y 
+  echo "Python3-pip was installed successfully."
+else
+  echo "Item were already installed."
+fi 
+
+# Launch ansible playbook
+ansible-playbook packages_to_install.yml
