81f69b16511df057aa103ce1143609d9de142969
c-poteat <nellp7@yahoo.com>
2023-12-24 10:06:11 -0500

    updated

--- ansible-core-2.14_install.sh
+++ ansible-core-2.14_install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sudo dnf remove python36
+# If installing via Ansible Tower/AAP, there is no need to run additional configurations.
+# If installing via remote server, then you need to run sudo ansible-playbook linux_remote_setup.yml after this script.
 
-sudo yum install python39
+sudo dnf remove -y python36
+
+sudo yum install -y python39
 
 # # Create a virtual environment
 # python3 -m venv ansible-env
@@ -17,3 +20,4 @@ pip3 install ansible-core==2.14
 ansible-galaxy collection install ansible.windows
 ansible-galaxy collection install community.windows
 ansible-galaxy collection install microsoft.ad
+
